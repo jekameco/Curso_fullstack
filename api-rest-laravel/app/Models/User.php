3c536e9c665431a8dd74635e20540c5d14bb6e65
Jekameco <mesakathe31@gmail.com>
2025-05-23 10:55:54 -0500

    add models to laravel

--- api-rest-laravel/app/Models/User.php
+++ api-rest-laravel/app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'description',
         'email',
         'password',
     ];
@@ -44,5 +46,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts(){
+        return $this->hasMany('App\Models\Post');
     }
 }
